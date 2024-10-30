70f619443c316bf5b22852c913a7f2f614e3d138
Ptalentajieh <talented037l@gmail.com>
2024-10-30 17:04:20 +0100

    Third commit

---
+++ week-3/practice_6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\talen\Onedrive\Documents\Github\P.talent-ajiehCSC-101\week-3\practice_6\target\debug\deps\practice_6.exe: src/main.rs
+
+C:\Users\talen\Onedrive\Documents\Github\P.talent-ajiehCSC-101\week-3\practice_6\target\debug\deps\practice_6.d: src/main.rs
+
+src/main.rs:
